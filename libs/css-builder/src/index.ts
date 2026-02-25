6d3a30a521bb676d50e440176f69d990143f9029
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2026-02-25 12:02:51 +0100

    Use css-builder from jsr

--- libs/css-builder/src/index.ts
+++
@@ -1,6 +0,0 @@
-import type * as Ast from "./ast";
-import * as create from "./create";
-
-export * from "./builder";
-export * from "./serialize";
-export { Ast, create };
--- libs/css-builder/tsdown.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "tsdown";
-
-export default defineConfig({
-  entry: ["./src/index.ts", "./src/ast.ts", "./src/create.ts", "./src/builder.ts", "./src/serialize.ts"],
-  platform: "neutral",
-  dts: true,
-  exports: true,
-});
